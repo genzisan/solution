953bc712e7ce1fb0a2347fdd1decbbe9f9a5c968
genizi <73005681+genizi@users.noreply.github.com>
2020-11-25 16:00:11 +0900

    index,new,createまでのコントローラー編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,27 @@
 class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    tweet = Tweet.new(tweet_params)
+    if tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:text, :category_id).merge(user_id: current_user.id)
+  end
+
+
 end
