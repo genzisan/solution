7f1a071250e0971dfa6fabaa0956e06f6a689c14
genizi <73005681+genizi@users.noreply.github.com>
2020-11-28 13:53:40 +0900

    tweetコントローラー
    末ログイン者の制限と、
    各種アクションの記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -16,6 +17,39 @@ class TweetsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @tweet =Tweet.find(params[:id]) 
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user).order("created_at DESC")
+  end
+
+  def edit
+    tweet = Tweet.find(params[:id])
+    if user_signed_in? && current_user.id == tweet.user_id
+      @tweet =Tweet.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(tweet_params)
+      redirect_to tweet_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
 
   private
 
